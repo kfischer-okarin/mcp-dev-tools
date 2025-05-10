69eff4b209906beedab2a54c559563e880bc2e97
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-10 14:24:49 +0900

    Move generated string to instance variable
    So we can start extracting private methods

--- lib/json_schema_class_generator.rb
+++ lib/json_schema_class_generator.rb
@@ -3,27 +3,32 @@
 class JsonSchemaClassGenerator
   def initialize(schema)
     @schema = schema
+    @result = +""
+    @generated = false
   end
 
   def generate
-    result = +""
-    @schema["definitions"].each.with_index do |(class_name, class_schema), index|
-      next unless class_schema.key?("properties") # TODO: Handle allOf
+    unless @generated
+      @schema["definitions"].each.with_index do |(class_name, class_schema), index|
+        next unless class_schema.key?("properties") # TODO: Handle allOf
 
-      properties = class_schema["properties"].keys
-      define_args = properties.map { |prop| ":#{prop}" }.join(", ")
-      if class_schema["description"]
-        description_lines = class_schema["description"].split("\n")
-        description_lines.each do |line|
-          result << "# #{line}".strip << "\n"
+        properties = class_schema["properties"].keys
+        define_args = properties.map { |prop| ":#{prop}" }.join(", ")
+        if class_schema["description"]
+          description_lines = class_schema["description"].split("\n")
+          description_lines.each do |line|
+            @result << "# #{line}".strip << "\n"
+          end
         end
+        @result << "#{class_name} = Data.define(#{define_args})\n"
+        @result << "\n" if index < @schema["definitions"].size - 1
+      rescue => e
+        # Re-raise the exception with the class name for better debugging
+        raise e.class, "#{class_name}: #{e.message}"
       end
-      result << "#{class_name} = Data.define(#{define_args})\n"
-      result << "\n" if index < @schema["definitions"].size - 1
-    rescue => e
-      # Re-raise the exception with the class name for better debugging
-      raise e.class, "#{class_name}: #{e.message}"
+      @generated = true
     end
-    result
+
+    @result
   end
 end
