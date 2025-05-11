1b184121cea5c93cf86fbf98cb256be667ef8b98
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-12 01:03:38 +0900

    Generate dap classes

---
+++ scripts/generate_dap_classes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'json'
+require_relative '../lib/json_schema_class_generator'
+
+schema_path = File.expand_path('../vendor/debug-adapter-protocol/debugAdapterProtocol.json', __dir__)
+output_path = File.expand_path('../lib/dap.rb', __dir__)
+
+schema = JSON.parse(File.read(schema_path), symbolize_names: true)
+generator = JSONSchemaClassGenerator.new(schema)
+generated_code = generator.generate
+
+File.write(output_path, generated_code)
