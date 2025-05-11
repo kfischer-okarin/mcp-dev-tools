1e49be34186f8877e785ba660d74551635352f78
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-11 19:34:16 +0900

    Move specs and to test file

--- lib/json_schema_class_generator.rb
+++ lib/json_schema_class_generator.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-# SUMMARY: class JSONSchemaClassGenerator
-# SUMMARY: This class generates Data classes from a JSON schema.
-# SUMMARY: It is used to create classes for the Debug Adapter Protocol (DAP).
-# <spec>
-# - It takes a JSON schema as loaded via `JSON.load_file` with symbol keys as a single constructor argument.
-# - Generation will be done via a `generate` instance method which should return the generated Ruby code as a string.
-# - It only needs to be able to handle the schema at vendor/debug-adapter-protocol/debugAdapterProtocol.json
-# - It should create a value object class using `Data.define` for each definition in the schema.
-# </spec>
-
 class JSONSchemaClassGenerator
   def initialize(schema)
     @schema = schema
--- test/json_schema_class_generator_test.rb
+++ test/json_schema_class_generator_test.rb
@@ -4,6 +4,16 @@ require 'json'
 require_relative 'test_helper'
 require_relative '../lib/json_schema_class_generator'
 
+# SUMMARY: class JSONSchemaClassGenerator
+# SUMMARY: This class generates Data classes from a JSON schema.
+# SUMMARY: It is used to create classes for the Debug Adapter Protocol (DAP).
+# <spec>
+# - It takes a JSON schema as loaded via `JSON.load_file` with symbol keys as a single constructor argument.
+# - Generation will be done via a `generate` instance method which should return the generated Ruby code as a string.
+# - It only needs to be able to handle the schema at vendor/debug-adapter-protocol/debugAdapterProtocol.json
+# - It should create a value object class using `Data.define` for each definition in the schema.
+# </spec>
+
 class TestJSONSchemaClassGenerator < Minitest::Test
   def test_initialize_with_symbol_keys
     schema = { title: 'Example', type: 'object' }
