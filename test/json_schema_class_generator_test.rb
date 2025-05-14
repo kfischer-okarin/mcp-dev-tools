bbdc37efb2fc65ca3d890fab88a9184fcb249c3c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-14 14:24:41 +0900

    Slightly reword DAP schema requirement

--- test/json_schema_class_generator_test.rb
+++ test/json_schema_class_generator_test.rb
@@ -10,7 +10,8 @@ require_relative "../lib/json_schema_class_generator"
 # <spec>
 # - It takes a JSON schema as loaded via `JSON.load_file` with symbol keys as a single constructor argument.
 # - Generation will be done via a `generate` instance method which should return the generated Ruby code as a string.
-# - It only needs to be able to handle the schema at vendor/debug-adapter-protocol/debugAdapterProtocol.json
+# - It needs to be able to process the schema at vendor/debug-adapter-protocol/debugAdapterProtocol.json without
+#   errors.
 # - It should create a value object class using `Data.define` for each definition in the schema.
 # - Each generated data class should have all properties defined in the schema as attributes, the attributes should
 #   be in snake case. Be sure to handle consecutive capital letters correctly: JSONValue -> json_value
